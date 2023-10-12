c6c3468b852a0b03c43cf802794be7dbdf7d7a6f
jonw-cogapp <jonw@cogapp.com>
2023-10-12 10:35:43 +0100

    Tidying up and clarifications: - peer dependency seems a truer representation of what we expect - some vite config probably needed clarification - the bundle doesn't really belong in the assets dir

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,11 @@ export default defineConfig({
       name: "CustomTourBuilder",
     },
     rollupOptions: {
+      // We don't want to bundle React with our code, so we mark it as external
       external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
         assetFileNames: `assets/[name].[ext]`,
         globals: {
           react: "react",
@@ -24,7 +25,15 @@ export default defineConfig({
       },
     },
   },
+  // This option avoids bundling excess code.
+  // It seems related to React 17+ changing the JSX runtime
+  // and may need to be re-evaluated if we upgrade React
+  // See:
+  // - https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+  // - https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md#jsxruntime
+  // - https://www.npmjs.com/package/@vitejs/plugin-react
   plugins: [react({ jsxRuntime: "classic" })],
+  // Have static port for documentation purposes
   server: {
     port: 43110,
     strictPort: true,
