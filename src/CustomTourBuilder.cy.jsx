0042f17ea8ac2fc9b7eea9447f81e67e1fd8470f
jonw-cogapp <jonw@cogapp.com>
2023-10-16 15:18:52 +0100

    Fix tests on CustomTourBuilder

--- src/CustomTourBuilder.cy.jsx
+++ src/CustomTourBuilder.cy.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import CustomTourBuilder from "./CustomTourBuilder";
+import { AppProvider } from "./contexts/AppContext";
+import { SearchProvider } from "./contexts/SearchContext";
 
 describe("<CustomTourBuilder />", () => {
   it("Renders", () => {
-    cy.mount(<CustomTourBuilder />);
+    cy.mount(
+      <AppProvider>
+        <SearchProvider>
+          <CustomTourBuilder />
+        </SearchProvider>
+      </AppProvider>,
+    );
     cy.get("#aic-ct-search").should("exist");
   });
 
   it("Can perform a search and show results", () => {
     // Use intercept to ping the search api and use the search.json fixture
     // but wait while we check the loading message
-    cy.intercept(
-      "GET",
-      "https://api.artic.edu/api/v1/artworks/search*",
-      (req) => {
-        cy.get("#aic-ct-search__loading").should("have.text", "Loading...");
-        req.reply({
-          fixture: "json/search.json",
-        });
-      },
-    );
+    cy.intercept("GET", "https://api.artic.edu/api/v1/artworks/search*", {
+      fixture: "json/search.json",
+      delayMs: 80,
+    });
 
-    cy.mount(<CustomTourBuilder />);
+    cy.mount(
+      <AppProvider>
+        <SearchProvider>
+          <CustomTourBuilder />
+        </SearchProvider>
+      </AppProvider>,
+    );
     cy.get("#aic-ct-search__input").type("test");
     cy.get("#aic-ct-search__button").click();
+    cy.get("#aic-ct-search__loading").should("have.text", "Loading...");
     cy.get("#aic-ct-search__results").should("exist");
     cy.get("#aic-ct-search__results li").should("have.length", 10);
   });
@@ -33,7 +42,13 @@ describe("<CustomTourBuilder />", () => {
       statusCode: 500,
     }).as("500");
 
-    cy.mount(<CustomTourBuilder />);
+    cy.mount(
+      <AppProvider>
+        <SearchProvider>
+          <CustomTourBuilder />
+        </SearchProvider>
+      </AppProvider>,
+    );
     cy.get("#aic-ct-search__input").type("test");
     cy.get("#aic-ct-search__button").click();
     cy.get("#aic-ct-search__error").should(
