1edf4323201a2c59fb35b8744bb2bb1fbb319290
jonw-cogapp <jonw@cogapp.com>
2023-10-17 14:59:35 +0100

    Update AppContext: Adds a reudcer for adding and removing items Provide tourItems and a dispatch function

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,7 +1,19 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 import PropTypes from "prop-types";
 
 export const AppContext = createContext();
+
+const tourItemsReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      // Insertion structure is [id(key), payload(value)]
+      return new Map([...state, [action.payload.id, action.payload]]);
+    case "REMOVE_ITEM":
+      return new Map([...state].filter(([key]) => key !== action.payload));
+    default:
+      return state;
+  }
+};
 
 /**
  * AppProvider
@@ -9,6 +21,9 @@ export const AppContext = createContext();
 export function AppProvider({ children }) {
   const [tourTitle, setTourTitle] = useState("");
   const [tourDescription, setTourDescription] = useState("");
+  // Although we could use an Array using a Map allows us to
+  // use the id as the key and makes the value easier to access
+  const [tourItems, dispatch] = useReducer(tourItemsReducer, new Map());
 
   return (
     <AppContext.Provider
@@ -18,6 +33,8 @@ export function AppProvider({ children }) {
         setTourTitle,
         tourDescription,
         setTourDescription,
+        tourItems,
+        tourItemsDispatch: dispatch,
       }}
     >
       {children}
@@ -32,5 +49,7 @@ AppProvider.propTypes = {
 AppContext.Provider.propTypes = {
   value: PropTypes.shape({
     iiifBaseUrl: PropTypes.string,
+    tourItems: PropTypes.instanceOf(Map),
+    tourItemsDispatch: PropTypes.func,
   }),
 };
