c3f555fba580a8e71940e1419202e3500ba95dcc
jonw-cogapp <jonw@cogapp.com>
2023-10-18 11:40:21 +0100

    Make fixture for tours be representative These are for real items so they should match

--- src/components/TourItems.cy.jsx
+++ src/components/TourItems.cy.jsx
@@ -17,15 +17,26 @@ describe("<TourItems />", () => {
   it("Renders with items", () => {
     cy.intercept(
       "GET",
-      "https://artic.edu/iiif/2/*/full/!240,240/0/default.jpg",
-      (req) => {
-        req.reply({
-          fixture: `../../cypress/fixtures/images/image_${
-            Math.floor(Math.random() * 10) + 1
-          }.jpg`,
-        });
+      "https://artic.edu/iiif/2/89300e26-55e8-ca20-9422-24c721192216/full/!240,240/0/default.jpg",
+      {
+        fixture: `../../cypress/fixtures/images/image_4.jpg`,
       },
     );
+    cy.intercept(
+      "GET",
+      "https://artic.edu/iiif/2/3e61c05a-fb22-6703-cc41-d4334000fb7e/full/!240,240/0/default.jpg",
+      {
+        fixture: `../../cypress/fixtures/images/image_5.jpg`,
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://artic.edu/iiif/2/c37cfb73-5985-e622-2346-90d3df8aba34/full/!240,240/0/default.jpg",
+      {
+        fixture: `../../cypress/fixtures/images/image_6.jpg`,
+      },
+    );
+
     cy.mount(
       <AppProvider tourItems={tourItems}>
         <TourItems />
