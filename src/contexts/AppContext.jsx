48d587df7c584b3f7c5b26c30a1ef714c928b1fd
jonw-cogapp <jonw@cogapp.com>
2023-10-18 11:58:45 +0100

    Add the required state for the new fields

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import PropTypes from "prop-types";
 
 export const AppContext = createContext();
@@ -7,10 +7,17 @@ export const AppContext = createContext();
  * AppProvider
  */
 export function AppProvider({ children }) {
+  const [tourTitle, setTourTitle] = useState("");
+  const [tourDescription, setTourDescription] = useState("");
+
   return (
     <AppContext.Provider
       value={{
         iiifBaseUrl: "https://artic.edu/iiif/2",
+        tourTitle,
+        setTourTitle,
+        tourDescription,
+        setTourDescription,
       }}
     >
       {children}
