47b9ad2546fbfc079fe7613cfa6bef906143c6d5
jonw-cogapp <jonw@cogapp.com>
2023-10-18 12:10:57 +0100

    Wire the inputs into the state

--- src/components/TourMetadata.jsx
+++ src/components/TourMetadata.jsx
@@ -7,18 +7,41 @@ import { AppContext } from "../contexts/AppContext";
  */
 
 function TourMetadata() {
+  const { tourTitle, setTourTitle, tourDescription, setTourDescription } =
+    useContext(AppContext);
+
   return (
     <>
       <div>
-        <label htmlFor="aic-ct-metadata__title">Tour Title</label>
+        <label htmlFor="aic-ct-metadata__title">
+          Tour Title{" "}
+          <span>
+            (255<span className="sr-only"> characters left</span>)
+          </span>
+        </label>
         <br />
-        <input type="text" id="aic-ct-metadata__title" />
+        <input
+          type="text"
+          onChange={(e) => setTourTitle(e.target.value)}
+          value={tourTitle}
+          id="aic-ct-metadata__title"
+        />
       </div>
 
       <div>
-        <label htmlFor="aic-ct-metadata__description">Tour Description</label>
+        <label htmlFor="aic-ct-metadata__description">
+          Tour Description{" "}
+          <span>
+            (255<span className="sr-only"> characters left</span>)
+          </span>
+        </label>
         <br />
-        <textarea id="aic-ct-metadata__description" rows={5} />
+        <textarea
+          id="aic-ct-metadata__description"
+          onChange={(e) => setTourDescription(e.target.value)}
+          rows="5"
+          defaultValue={tourDescription}
+        />
       </div>
     </>
   );
