329e78483425f877754015fbb7d322205e013451
jonw-cogapp <jonw@cogapp.com>
2023-10-18 12:19:59 +0100

    Add a utility for the character count

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,15 @@ export function iiifUrl(
 ) {
   return `${base}/${id}/${size}/${fit && "!"}${width},${height}/0/default.jpg`;
 }
+
+/**
+ * Characters Remaining
+ * Takes a string and a maximum length and returns the number of characters remaining
+ *
+ * @param {string} string - the string to be checked
+ * @param {number} maxLength - the maximum length of the string
+ * @returns {number} - the number of characters remaining
+ */
+export function charactersRemaining(string, maxLength) {
+  return maxLength - string.length;
+}
