a1b66c682acd17528b420d95273b78a30f2ec3a5
jonw-cogapp <jonw@cogapp.com>
2023-10-20 20:23:29 +0100

    Add focus handling This was way harder than i'd imagined

--- src/components/TourItem.jsx
+++ src/components/TourItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { iiifUrl } from "../utils";
 import { AppContext } from "../contexts/AppContext";
 import PropTypes from "prop-types";
@@ -7,16 +7,62 @@ import PropTypes from "prop-types";
  * TourItem
  */
 function TourItem(props) {
-  const { itemData } = props;
-  const { iiifBaseUrl, tourItemsDispatch } = useContext(AppContext);
+  const { itemData, setShouldAssignFocus, setRemoveButtons } = props;
+  const { iiifBaseUrl, tourItems, tourItemsDispatch } = useContext(AppContext);
+  const buttonRef = useRef(null);
 
-  const handleClick = (id) => {
+  const handleClick = () => {
     // Remove the item from the tour
     tourItemsDispatch({
       type: "REMOVE_ITEM",
-      payload: id,
+      payload: itemData.id,
     });
   };
+
+  // When tourItems change check if this item was removed
+  useEffect(() => {
+    // This needs to be defined outside of the callback so that it can be used in the cleanup function
+    const ref = buttonRef.current;
+
+    return () => {
+      // Infer an item was removed and that focus should be reassigned if the remove button has focus
+      // This is more likely to be the case than comparing the length of the tourItems array
+      // as items can be toggled from the search screen
+      if (document.activeElement === ref) {
+        if (tourItems.length > 1) {
+          // Calculate which the next item to focus on should be using the id
+          // This should be the item that was after the one that was removed (or the last item)
+          tourItems.find((item, index) => {
+            if (item.id === itemData.id) {
+              setShouldAssignFocus({
+                flag: true,
+                id: tourItems[
+                  index !== tourItems.length - 1 ? index + 1 : index - 1
+                ].id,
+              });
+            }
+          });
+        } else {
+          // Focus on the "no results" button
+          setShouldAssignFocus({
+            flag: true,
+            id: null,
+          });
+        }
+      }
+    };
+  }, [tourItems, itemData.id, setShouldAssignFocus]);
+
+  // Add remove button ref to the array of remove buttons
+  // This is used externall to move focus to the next item when an item is removed
+  useEffect(() => {
+    setRemoveButtons((prev) => [...prev, { id: itemData.id, ref: buttonRef }]);
+    return () => {
+      setRemoveButtons((prev) =>
+        prev.filter((item) => item.id !== itemData.id),
+      );
+    };
+  }, [setRemoveButtons, tourItems, itemData.id]);
 
   return (
     <li id={`aic-ct-tour__item-${itemData.id}`}>
@@ -33,6 +79,7 @@ function TourItem(props) {
         <div dangerouslySetInnerHTML={{ __html: itemData.description }}></div>
       )}
       <button
+        ref={buttonRef}
         type="button"
         onClick={() => {
           handleClick(itemData.id);
@@ -55,6 +102,8 @@ TourItem.propTypes = {
     artist_title: PropTypes.string,
     description: PropTypes.string,
   }),
+  setRemoveButtons: PropTypes.func,
+  setShouldAssignFocus: PropTypes.func,
 };
 
 export default TourItem;
--- src/components/TourItems.jsx
+++ src/components/TourItems.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import TourItem from "./TourItem";
 import { AppContext } from "../contexts/AppContext";
 
@@ -6,21 +6,65 @@ import { AppContext } from "../contexts/AppContext";
  * SearchResults
  */
 function TourItems() {
-  const { tourItems } = useContext(AppContext);
+  const { tourItems, setActiveNavPage } = useContext(AppContext);
+  const [shouldAssignFocus, setShouldAssignFocus] = useState({
+    flag: false,
+    id: null,
+  });
+  const [removeButtons, setRemoveButtons] = useState([]);
+  const noResultButtonRef = useRef(null);
+
+  // When tourItems change check if this item was removed
+  // Focus will always need to be reassigned in some way in that instance
+  useEffect(() => {
+    if (shouldAssignFocus.flag) {
+      if (noResultButtonRef?.current) {
+        // When there's no items in the tour, focus on the "no results" button
+        noResultButtonRef.current.focus();
+      } else {
+        // Otherwise focus on the element passed out by the removed item
+        removeButtons
+          .find((item) => item.id === shouldAssignFocus.id)
+          .ref.current.focus();
+      }
+
+      setShouldAssignFocus(false);
+    }
+  }, [tourItems, shouldAssignFocus, removeButtons]);
 
   // Render the results if there are results
   return (
     <>
-      {tourItems.length > 0 && (
+      {tourItems.length > 0 ? (
         <>
           <h2 id="aic-ct-tour__heading">Your tour</h2>
           <ul id="aic-ct-tour__results">
             {/* JSX cannot directly render a Map and it must be converted to an Array */}
             {tourItems.map((itemData) => (
-              <TourItem key={itemData.id} itemData={itemData} />
+              <TourItem
+                key={itemData.id}
+                setRemoveButtons={setRemoveButtons}
+                itemData={itemData}
+                shouldAssignFocus={shouldAssignFocus}
+                setShouldAssignFocus={setShouldAssignFocus}
+              />
             ))}
           </ul>
         </>
+      ) : (
+        <div id="aic-ct-tour__no-items">
+          You haven’t added any items to your tour yet
+          <button
+            ref={noResultButtonRef}
+            id="aic-ct-tour__no-items-search-button"
+            type="button"
+            onClick={() => {
+              setActiveNavPage(0);
+            }}
+          >
+            Search
+          </button>
+        </div>
       )}
     </>
   );
