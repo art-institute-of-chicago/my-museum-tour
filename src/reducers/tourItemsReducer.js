404683e5ef24a61e105edb4a4877eb169d8cb808
jonw-cogapp <jonw@cogapp.com>
2023-10-20 15:52:16 +0100

    Add comments to reducer

--- src/reducers/tourItemsReducer.js
+++ src/reducers/tourItemsReducer.js
@@ -8,8 +8,10 @@ const tourItemsReducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
       // Insertion structure is [id(key), payload(value)]
+      // Payload for add is the entire item JSON
       return new Map([...state, [action.payload.id, action.payload]]);
     case "REMOVE_ITEM":
+      // Payload for remove is the id, so filter out non-matching ids
       return new Map([...state].filter(([key]) => key !== action.payload));
     default:
       return state;
