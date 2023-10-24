4c83dc98f59b931687ad6893b639cd07d13f26b9
jonw-cogapp <jonw@cogapp.com>
2023-10-24 12:29:05 +0100

    Replace character limit logic with hook

--- src/components/tour/TourMetadata.jsx
+++ src/components/tour/TourMetadata.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useEffect } from "react";
 import { AppContext } from "../../contexts/AppContext";
-import { charactersRemaining } from "../../utils";
+import useCappedInput from "../../hooks/useCappedInput";
 
 /**
  * TourMetadata
@@ -11,66 +11,53 @@ function TourMetadata() {
   const { tourTitle, setTourTitle, tourDescription, setTourDescription } =
     useContext(AppContext);
   // You may wish to make this smaller for debugger
-  const maxLength = 255;
-  const titleCountRef = useRef(null);
-  const descriptionCountRef = useRef(null);
+  const cappedTitle = useCappedInput(tourTitle, 255);
+  const cappedDescription = useCappedInput(tourDescription, 255);
 
-  // Update the state when inputs change
-  const handleChange = ({ setter, countRef, e }) => {
-    // Let AT know things are in progress to limit excessive announcements while typing
-    countRef.current.ariaBusy = true;
-    setter(e.target.value);
-    // Let AT know things are done
-    countRef.current.ariaBusy = false;
-  };
+  // Update tourTitle and tourDescription when cappedTitle and cappedDescription change
+  useEffect(() => {
+    setTourTitle(cappedTitle.value);
+  }, [cappedTitle, setTourTitle]);
+
+  useEffect(() => {
+    setTourDescription(cappedDescription.value);
+  }, [cappedDescription, setTourDescription]);
 
   return (
     <>
       <div>
         <label htmlFor="aic-ct-metadata__title">
           Tour Title{" "}
-          <span ref={titleCountRef} aria-live="polite">
-            ({charactersRemaining(tourTitle, maxLength)}
+          <span ref={cappedTitle.countRef} aria-live="polite">
+            ({cappedTitle.charsRemaining}
             <span className="sr-only"> characters remaining</span>)
           </span>
         </label>
         <br />
         <input
           type="text"
-          onChange={(e) =>
-            handleChange({
-              setter: setTourTitle,
-              countRef: titleCountRef,
-              e,
-            })
-          }
-          value={tourTitle}
+          onChange={cappedTitle.onChange}
+          value={cappedTitle.value}
           id="aic-ct-metadata__title"
-          maxLength={maxLength}
+          maxLength={cappedTitle.maxLength}
         />
       </div>
 
       <div>
         <label htmlFor="aic-ct-metadata__description">
           Tour Description{" "}
-          <span ref={descriptionCountRef} aria-live="polite">
-            ({charactersRemaining(tourDescription, maxLength)}
+          <span ref={cappedDescription.countRef} aria-live="polite">
+            ({cappedDescription.charsRemaining}
             <span className="sr-only"> characters remaining</span>)
           </span>
         </label>
         <br />
         <textarea
           id="aic-ct-metadata__description"
-          onChange={(e) =>
-            handleChange({
-              setter: setTourDescription,
-              countRef: descriptionCountRef,
-              e,
-            })
-          }
+          onChange={cappedDescription.onChange}
           rows="5"
-          value={tourDescription}
-          maxLength={maxLength}
+          value={cappedDescription.value}
+          maxLength={cappedDescription.maxLength}
         />
       </div>
     </>
