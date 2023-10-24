acbec1bb9c106c873a10fe3ce9f9e5526dab49c7
jonw-cogapp <jonw@cogapp.com>
2023-10-24 17:15:34 +0100

    Add note to component test

--- src/components/tour/TourItem.cy.jsx
+++ src/components/tour/TourItem.cy.jsx
@@ -35,4 +35,53 @@ describe("<TourItem />", () => {
       )
       .should("have.attr", "alt", "Test image alt text");
   });
+
+  it("Updates the character count", () => {
+    cy.mount(
+      <AppProvider>
+        <TourItem
+          key={item.id}
+          itemData={item}
+          itemIndex={0}
+          setRemoveButtons={() => {}}
+          setShouldAssignFocus={() => {}}
+        />
+      </AppProvider>,
+    );
+    cy.get("label[for=aic-ct-note-1]").should(
+      "have.text",
+      "Personal note (255 characters remaining)",
+    );
+    cy.get("#aic-ct-note-1").should("exist").type("Lorem ipsum dolor sit amet");
+    cy.get("label[for=aic-ct-note-1]").should(
+      "have.text",
+      "Personal note (229 characters remaining)",
+    );
+  });
+
+  it("Doesn't allow more than the character limit", () => {
+    const noteText =
+      "Nullam aliquet fringilla dolor, vitae malesuada massa rutrum eget. Quisque sed nibh augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque tristique finibus sapien, condimentum condimentum magna biam.";
+
+    cy.mount(
+      <AppProvider>
+        <TourItem
+          key={item.id}
+          itemData={item}
+          itemIndex={0}
+          setRemoveButtons={() => {}}
+          setShouldAssignFocus={() => {}}
+        />
+      </AppProvider>,
+    );
+    // This is 257 characters including spaces and punctuation
+    cy.get("#aic-ct-note-1").type(noteText, { delay: 0 });
+
+    // 255 - 257 = -2 (0)
+    cy.get('label[for="aic-ct-note-1"]').should(
+      "have.text",
+      "Personal note (0 characters remaining)",
+    );
+    cy.get("#aic-ct-note-1").should("have.value", noteText.slice(0, 255));
+  });
 });
