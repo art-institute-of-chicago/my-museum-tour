db315f61b0d65a7497cb976edac175979f9dc1fc
jonw-cogapp <jonw@cogapp.com>
2023-11-15 12:27:33 +0000

    Update form elements

--- src/components/tour/TourMetadata.jsx
+++ src/components/tour/TourMetadata.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../contexts/AppContext";
 import useCappedInput from "../../hooks/useCappedInput";
 
@@ -11,18 +11,39 @@ function TourMetadata() {
   const {
     tourTitle,
     setTourTitle,
+    creatorEmail,
+    setCreatorEmail,
+    creatorName,
+    setCreatorName,
+    recipientName,
+    setRecipientName,
+    marketingOptIn,
+    setMarketingOptIn,
     tourDescription,
     setTourDescription,
     limits,
   } = useContext(AppContext);
   // You may wish to make this smaller for debugger
   const cappedTitle = useCappedInput(tourTitle, limits.title);
+  const cappedCreatorName = useCappedInput(creatorName, limits.creatorName);
+  const cappedRecipientName = useCappedInput(
+    recipientName,
+    limits.recipientName,
+  );
   const cappedDescription = useCappedInput(tourDescription, limits.description);
 
-  // Update tourTitle and tourDescription when cappedTitle and cappedDescription change
+  // Update values in context when capped input changes
   useEffect(() => {
     setTourTitle(cappedTitle.value);
   }, [cappedTitle, setTourTitle]);
+
+  useEffect(() => {
+    setCreatorName(cappedCreatorName.value);
+  }, [cappedCreatorName, setCreatorName]);
+
+  useEffect(() => {
+    setRecipientName(cappedRecipientName.value);
+  }, [cappedRecipientName, setRecipientName]);
 
   useEffect(() => {
     setTourDescription(cappedDescription.value);
@@ -45,13 +66,65 @@ function TourMetadata() {
           value={cappedTitle.value}
           id="aic-ct-metadata__title"
           maxLength={cappedTitle.maxLength}
+          aria-required="true"
           required
         />
       </div>
 
       <div>
+        <label htmlFor="aic-ct-metadata__creator-name">
+          Your name <em>(optional)</em>{" "}
+          <span ref={cappedCreatorName.countRef} aria-live="polite">
+            ({cappedCreatorName.charsRemaining}
+            <span className="sr-only"> characters remaining</span>)
+          </span>
+        </label>
+        <br />
+        <input
+          type="text"
+          value={cappedCreatorName.value}
+          onChange={cappedCreatorName.onChange}
+          id="aic-ct-metadata__creator-name"
+          maxLength={cappedCreatorName.maxLength}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="aic-ct-metadata__creator-email">Your email</label>
+        <br />
+        <input
+          type="email"
+          value={creatorEmail.value}
+          onChange={(e) => {
+            setCreatorEmail(e.target.value);
+          }}
+          id="aic-ct-metadata__creator-email"
+          aria-required="true"
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="aic-ct-metadata__recipient-name">
+          Who is this tour for? <em>(optional)</em>{" "}
+          <span ref={cappedRecipientName.countRef} aria-live="polite">
+            ({cappedRecipientName.charsRemaining}
+            <span className="sr-only"> characters remaining</span>)
+          </span>
+        </label>
+        <br />
+        <input
+          type="text"
+          value={cappedRecipientName.value}
+          onChange={cappedRecipientName.onChange}
+          id="aic-ct-metadata__recipient-name"
+          maxLength={cappedRecipientName.maxLength}
+        />
+      </div>
+
+      <div>
         <label htmlFor="aic-ct-metadata__description">
-          Tour Description{" "}
+          Tour Description <em>(optional)</em>{" "}
           <span ref={cappedDescription.countRef} aria-live="polite">
             ({cappedDescription.charsRemaining}
             <span className="sr-only"> characters remaining</span>)
@@ -65,6 +138,20 @@ function TourMetadata() {
           value={cappedDescription.value}
           maxLength={cappedDescription.maxLength}
         />
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          id="aic-ct-metadata__opt-in"
+          checked={marketingOptIn}
+          onChange={(e) => {
+            setMarketingOptIn(e.target.checked);
+          }}
+        />
+        <label htmlFor="aic-ct-metadata__opt-in">
+          I would like to recieve marketing emails from AIC
+        </label>
       </div>
     </>
   );
