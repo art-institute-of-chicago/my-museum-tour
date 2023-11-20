fa6cf282f789257f68cd61bf6ee54b5a622e93cc
jonw-cogapp <jonw@cogapp.com>
2023-11-20 16:04:26 +0000

    Update header classes for styling

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -16,23 +16,28 @@ function Header() {
   return (
     <header
       id="aic-ct-header"
-      className="aic-ct-header"
+      className="aic-ct-header f-body"
       aria-label="Custom tour builder"
     >
-      <div className="aic-ct__item-info" aria-live="polite">
-        <span id="aic-ct-item-count" className="aic-ct__item-count">
+      <div className="aic-ct-item-info" aria-live="polite">
+        <span id="aic-ct-item-count" className="aic-ct-item-count">
           {items}
         </span>{" "}
-        artworks added <em>(of max 6)</em>
+        <span>
+          artworks added <em>(of max 6)</em>
+        </span>
       </div>
       <button
         id="aic-ct-header__button"
-        className="aic-ct-header__button"
+        className="aic-ct-header__button btn btn--transparent btn--w-icon f-buttons"
         type="button"
         onClick={handleClick}
       >
         {activeNavPage === 0 && "Preview"}
         {activeNavPage > 0 && "Finish"}
+        <svg className="icon--arrow" aria-hidden="true">
+          <use xlinkHref="#icon--arrow"></use>
+        </svg>
       </button>
     </header>
   );
