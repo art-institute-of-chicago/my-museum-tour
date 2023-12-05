db3c59e91f6e8be1d15047f72d875d5a04c4d3ad
jonw-cogapp <jonw@cogapp.com>
2023-12-05 16:48:43 +0000

    Refactor to have common element when rendering Might not be needed, but feels good to have

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -44,62 +44,61 @@ function SearchResults() {
     }, 0);
   }, [searchResultItems]);
 
-  // Render only the loading message while fetching
-  if (searchFetching) {
-    return <div id="aic-ct-search__loading">Loading...</div>;
+  // Catch all for no results, error, and loading states
+  if (!searchResultItems && !searchFetching && !searchError) {
+    return null;
   }
 
-  // Render only the error message if there is an error
-  if (searchError) {
-    return <div id="aic-ct-search__error">{searchError}</div>;
-  }
-
-  // Render only a no results message if there are no results
-  if (searchResultItems?.length === 0) {
-    return (
-      <div id="aic-ct-search__no-results">
-        Sorry, we couldn’t find any results matching your criteria
-      </div>
-    );
-  }
-
-  // Render the results if there are results
-  if (searchResultItems?.length > 0) {
-    return (
-      <>
-        <header className="aic-ct-section-header f-body">
-          <h2 className="f-module-title-2">Choose artworks</h2>
-          <span className="aic-ct-item-count aic-ct-item-count--body">
-            {searchResultItems.length}
-          </span>
-        </header>
-        <p className="aic-ct-pre-result-text f-body">
-          Browse these artworks currently on view and available for your tour.
-        </p>
-        <ul
-          id="aic-ct-search__results"
-          className="o-pinboard o-pinboard--2-col@xsmall o-pinboard--2-col@small o-pinboard--3-col@medium o-pinboard--4-col@large o-pinboard--4-col@xlarge"
-          data-pinboard-option-layout="o-pinboard--2-col@xsmall o-pinboard--2-col@small o-pinboard--2-col@medium o-pinboard--3-col@large o-pinboard--3-col@xlarge"
-          data-pinboard-maintain-order="false"
-          data-behavior="pinboard"
-        >
-          {searchResultItems.map((itemData) => (
-            <SearchResultItem key={itemData.id} itemData={itemData} />
-          ))}
-        </ul>
-        <dialog
-          ref={searchPreviewRef}
-          id="aic-ct-search-preview"
-          onClose={handleClose}
-        >
-          <SearchPreview />
-        </dialog>
-      </>
-    );
-  }
-
-  // Catch all
-  return null;
+  return (
+    <div className="aic-ct-search-results">
+      {searchFetching && (
+        // Render only the loading message while fetching
+        <div id="aic-ct-search-results__loading">Loading...</div>
+      )}
+      {searchError && (
+        // Render only the error message if there is an error
+        <div id="aic-ct-search-results__error">{searchError}</div>
+      )}
+      {searchResultItems?.length === 0 && !searchFetching && !searchError && (
+        // Render only a no results message if there are no results
+        <div id="aic-ct-search-results__no-results">
+          Sorry, we couldn’t find any results matching your criteria
+        </div>
+      )}
+      {searchResultItems?.length > 0 && !searchFetching && !searchError && (
+        // Render the results if there are results
+        <>
+          <header className="aic-ct-section-header f-body">
+            <h2 className="f-module-title-2">Choose artworks</h2>
+            <span className="aic-ct-item-count aic-ct-item-count--body">
+              {searchResultItems.length}
+            </span>
+          </header>
+          <p className="aic-ct-pre-result-text f-body">
+            Browse these artworks currently on view and available for your tour.
+          </p>
+          <ul
+            id="aic-ct-search-results__items"
+            className="o-pinboard o-pinboard--2-col@xsmall o-pinboard--2-col@small o-pinboard--3-col@medium o-pinboard--4-col@large o-pinboard--4-col@xlarge"
+            data-pinboard-option-layout="o-pinboard--2-col@xsmall o-pinboard--2-col@small o-pinboard--2-col@medium o-pinboard--3-col@large o-pinboard--3-col@xlarge"
+            data-pinboard-maintain-order="false"
+            data-behavior="pinboard"
+          >
+            {searchResultItems.map((itemData) => (
+              <SearchResultItem key={itemData.id} itemData={itemData} />
+            ))}
+          </ul>
+          <dialog
+            ref={searchPreviewRef}
+            id="aic-ct-search-preview"
+            onClose={handleClose}
+          >
+            <SearchPreview />
+          </dialog>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default SearchResults;
